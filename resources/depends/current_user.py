f80555e88c7d951fd38f6b2d65a6ce298e6bf6b0
user <ahmadboyabdurahimov589@gmail.com>
2025-03-19 15:55:54 +0500

    salom

--- resources/depends/current_user.py
+++ resources/depends/current_user.py
@@ -30,33 +30,11 @@ async def get_user_or_none(
         if user:
             if user.is_active is False:
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User is not active')
+            return user
 
-        return user
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User does not exist')
 
 
-async def get_active_user(
-        user: Annotated[User, Depends(get_user_or_none)],
-):
-    if user and user.is_active:
-        return user
-
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
-
-def get_user_by_role(**kwargs):
-    async def role_checker(
-            db: Annotated[AsyncSession, Depends(get_db)],
-            payload: Annotated[Payload, Depends(get_token_payload)],
-    ):
-        user = await User.repo.db_first(session=db, id=payload.id, **kwargs)
-        if user is None:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="You do not have permission to perform this action"
-            )
-        return user
-
-    return role_checker
 
 
 user_or_none = Annotated[User, Depends(get_user_or_none)]
-active_user = Annotated[User, Depends(get_active_user)]
